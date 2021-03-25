a82f35466f215bcad2691a367c95797e9242fc29
Aniela Govada <asgovada@gmail.com>
2021-03-25 14:46:43 -0600

    simple js

---
+++ code-journal.js
@@ -0,0 +1,36 @@
+/* Variables - containers that store values */
+
+var name // a decalred varible, but not initialized and its in the global scope
+
+var foo //a declared variable that can be changes
+
+const bar // a declared varible that cannot be changed - short for 'constant'
+
+const ANSWER = 42 // const is declared and initialized with the value 42
+
+// String 
+
+let string1 = 'Hello World!' // read '=' ad 'is assigned the calue of...'
+
+let string2 = "Hello Utah!"
+
+let string3 = new String('Hello World!')
+
+// Number
+
+let myNum = 2083972347
+
+let myNum2 = 75.43
+
+'1' == 1 // this statement is true because of type coercion and loose equality checking
+'1' === 1 // false because this is strict equality checking
+
+// Boolean 
+
+let myBool = true
+
+// Array
+
+let myArray = [] // this is an empty array
+
+let myArray2 = [42, 'Bob', myBool, ANSWER, true]
