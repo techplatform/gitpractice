00e811e8b050a707fbe800bc932f9c75140989fa
Aniela Govada <asgovada@gmail.com>
2021-04-22 17:16:38 -0600

    pokeImages

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -13,17 +13,18 @@ async function getAPIData(url){
     } 
     
     catch (error){
-    
-    console.log(error)
+        console.log(error)
     }
 }
 
 function loadPage(){
     const response = getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=25`).then(
-        (data)=> {
+        async(data)=> {
             for (const singlePokemon of data.results) {
-                console.log(singlePokemon)
-                populatePokeCard(singlePokemon)
+                await getAPIData(singlePokemon.url).then(
+                    (pokeData) => populatePokeCard(pokeData)
+                )
+                
             }
         }
     )
@@ -31,5 +32,48 @@ function loadPage(){
 }
 
 function populatePokeCard(singlePokemon){
-    console.log(singlePokemon)
+    //console.log(singlePokemon)
+    let pokeScene = document.createElement('div')
+    pokeScene.className = 'scene'
+    let pokeCard = document.createElement('div')
+    pokeCard.className = 'card'
+    pokeCard.addEventListener('click', () => {
+        pokeCard.classList.toggle('is-flipped')
+    })
+    pokeCard.appendChild(populateCardFront(singlePokemon))
+    pokeCard.appendChild(populateCardBack(singlePokemon))
+    pokeScene.appendChild(pokeCard)
+    pokeGrid.appendChild(pokeScene)
+}
+
+
+function populateCardFront (pokemon) {
+    console.log(pokemon)
+    let pokeFront = document.createElement('div')
+    pokeFront.className = 'card_face card_face--front'
+    let frontLabel = document.createElement('p')
+    frontLabel.textContent = pokemon.name
+    let frontImage = document.createElement('img')
+    frontImage.src = `images/${getImageFileName(pokemon)}.png`
+    
+    pokeFront.appendChild(frontLabel)
+    pokeFront.appendChild(frontImage)
+    return pokeFront
+}
+
+function populateCardBack(pokemon) {
+    let pokeBack = document.createElement('div')
+    pokeBack.className = 'card_face card_face--back'
+    let backLabel = document.createElement('p')
+    backLabel.textContent = `Moves: ${pokemon.moves.length}`
+    pokeBack.appendChild(backLabel)
+    return pokeBack
+}
+
+function getImageFileName(pokemon) {
+    if (pokemon.id < 10) {
+        return `00${pokemon.id}`
+    } else if (pokemon.id > 9 && pokemon.id < 100) {
+        return `0${pokemon.id}`
+    }
 }
